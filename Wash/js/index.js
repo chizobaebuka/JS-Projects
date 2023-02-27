6d4accd6970a7aaf5c1e0b538780212f851b5897
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-02-27 09:46:16 +0100

    added the functionality to the nav button to toggle switch it when clicked on a smaller screen size

--- Wash/js/index.js
+++ Wash/js/index.js
@@ -1,3 +1,18 @@
+
+// window.addEventListener("load", () => {
+
+//     const preloader = document.querySelector(".js-preloader");
+//     preloader.classList.add("fade-out");
+
+//     setTimeout(() => {
+//         preloader.style.display = "none";
+//          // animate on scroll
+//         AOS.init();
+//     }, 600)
+   
+// });
+
+
 /* header bg reveal*/
 const headerBg = () => {
     const header = document.querySelector(".js-header");
@@ -11,3 +26,27 @@ const headerBg = () => {
 } 
 
 headerBg();
+
+// nav
+const navigation = () => {
+    const navToggler = document.querySelector(".js-nav-toggler");
+    const nav = document.querySelector(".js-nav");
+    const navItem = nav.querySelectorAll("li");
+
+    const navToggle = () => {
+        nav.classList.toggle("open");
+        navToggler.classList.toggle("active");
+    }
+    navToggler.addEventListener("click", navToggle);
+
+    navItem.forEach((li) => {
+        li.querySelector("a").addEventListener("click", () => {
+            if(window.innerWidth <= 767){
+                navToggle();
+
+
+            }
+        })
+    });
+}
+navigation();
